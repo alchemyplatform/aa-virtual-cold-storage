45c49476b0971503285c5e0064713ad5ef8a3c29
David Philipson <david.philipson@gmail.com>
2024-03-04 14:40:24 -0800

    Add "install cold storage plugin" flow
    Adds a button that installs the cold storage plugin, then displays the
    secret key to the user. Also mints NFTs to the user in the process.

---
+++ utils/random.ts
@@ -0,0 +1,8 @@
+import { Hex } from 'viem';
+
+export function randomBytes32(): Hex {
+  const ints = new Uint32Array(8);
+  crypto.getRandomValues(ints);
+  const hexDigits = [...ints].map((n) => n.toString(16).padStart(8, '0')).join('');
+  return `0x${hexDigits}`;
+}
