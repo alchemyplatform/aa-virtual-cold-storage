a5efe44a8c017b6f833d5d3e84732e073b9fd6f1
Dennis Won <jhwon0820@gmail.com>
2024-03-04 15:56:20 -0800

    feat: nft assets main page and nft apis

--- hooks/assets.ts/useNftsForOwner.ts
+++ hooks/assets.ts/useNftsForOwner.ts
@@ -1,14 +1,15 @@
-import { useAccountContext } from '@/context/account';
-import { alchemy } from '@/utils/alchemy';
-import { alchemyEnhancedApiActions } from '@alchemy/aa-alchemy';
+import { getAlchemySettings } from '@/utils/alchemy';
+import { AlchemySmartAccountClient, alchemyEnhancedApiActions } from '@alchemy/aa-alchemy';
 import { useQuery } from '@tanstack/react-query';
+import { Alchemy } from 'alchemy-sdk';
 import { remove } from 'lodash';
 import { useMemo } from 'react';
 import { Address } from 'viem';
 
+const alchemy = new Alchemy(getAlchemySettings());
+
 // TODO replace with react-query infinite query for pagination
-const useNftsForOwner = ({ address }: { address: Address }) => {
-  const { client } = useAccountContext();
+const useNftsForOwner = ({ client, address }: { client: AlchemySmartAccountClient; address: Address }) => {
   const { data, isLoading, refetch } = useQuery({
     queryKey: ['nfts-for-owner', address],
     queryFn: async () =>
--- utils/alchemy.ts
+++ utils/alchemy.ts
@@ -1,7 +1,8 @@
-import { Alchemy, Network } from 'alchemy-sdk';
+import { env } from '@/env.mjs';
+import { AlchemySettings, Network } from 'alchemy-sdk';
 
 export function getRpcUrl() {
-  // This should run in the browser only. Without this check, the build fails.
+  // This should run in the client only. Without this check, the build fails.
   if (typeof window === 'undefined') {
     return '';
   }
@@ -9,7 +10,13 @@ export function getRpcUrl() {
   return `/api/rpc`;
 }
 
-export const alchemy = new Alchemy({
-  network: Network.ARB_SEPOLIA,
-  url: getRpcUrl()
-});
+export const getAlchemySettings = (): AlchemySettings => {
+  if (typeof window !== 'undefined') {
+    return {
+      network: Network.ARB_SEPOLIA,
+      url: getRpcUrl()
+    };
+  }
+
+  return { network: Network.ARB_SEPOLIA, apiKey: env.ALCHEMY_API_KEY };
+};
