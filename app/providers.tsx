653bf8f660ef2bd8e0147f78ec636a5041d28bf4
Dennis Won <jhwon0820@gmail.com>
2024-03-13 00:17:17 -0700

    fix: fix color mode toggle

--- app/providers.tsx
+++ app/providers.tsx
@@ -6,7 +6,15 @@ import { GlobalStyle } from '@/theme/globalstyles';
 import { default as theme } from '@/theme/theme';
 import { getRpcUrl } from '@/utils/alchemy';
 import { CacheProvider } from '@chakra-ui/next-js';
-import { ChakraProvider, ColorModeScript, Spinner, extendTheme } from '@chakra-ui/react';
+import {
+  ChakraProvider,
+  ColorModeProvider,
+  ColorModeScript,
+  Spinner,
+  ThemeProvider,
+  extendTheme
+} from '@chakra-ui/react';
+
 import { Global } from '@emotion/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Suspense, useState } from 'react';
@@ -30,22 +38,26 @@ export function Providers({ children }: { children: React.ReactNode }) {
 
   return (
     <CacheProvider>
-      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <ChakraProvider
-        theme={customTheme}
-        toastOptions={{ defaultOptions: { position: 'top-right', isClosable: true } }}
-      >
-        <Global styles={GlobalStyle} />
-        <QueryClientProvider client={queryClient}>
-          <Suspense
-            fallback={<Spinner thickness="4px" speed="0.75s" emptyColor="white" color="primary.500" size="xl" />}
+      <ThemeProvider theme={customTheme}>
+        <ColorModeProvider>
+          <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+          <ChakraProvider
+            theme={customTheme}
+            toastOptions={{ defaultOptions: { position: 'top-right', isClosable: true } }}
           >
-            <SignerContextProvider client={clientConfig}>
-              <GlobalModalProvider>{children}</GlobalModalProvider>
-            </SignerContextProvider>
-          </Suspense>
-        </QueryClientProvider>
-      </ChakraProvider>
+            <Global styles={GlobalStyle} />
+            <QueryClientProvider client={queryClient}>
+              <Suspense
+                fallback={<Spinner thickness="4px" speed="0.75s" emptyColor="white" color="primary.500" size="xl" />}
+              >
+                <SignerContextProvider client={clientConfig}>
+                  <GlobalModalProvider>{children}</GlobalModalProvider>
+                </SignerContextProvider>
+              </Suspense>
+            </QueryClientProvider>
+          </ChakraProvider>
+        </ColorModeProvider>
+      </ThemeProvider>
     </CacheProvider>
   );
 }
